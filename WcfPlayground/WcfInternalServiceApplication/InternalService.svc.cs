0426b93960964dd0ddcbd75e1e952517a8a11d1c
emily.jimmerson <57643494+Emijii@users.noreply.github.com>
2021-11-02 09:24:49 -0600

    Updating the internal service then calling it via the external service returns the update without having to update the service reference when debugging locally.

--- WcfPlayground/WcfInternalServiceApplication/InternalService.svc.cs
+++ WcfPlayground/WcfInternalServiceApplication/InternalService.svc.cs
@@ -4,12 +4,12 @@
     {
         public string GetMochi()
         {
-            return "Hi Mochi! :-) - Internal Service Called";
+            return "Hi Mochi! :-) - Internal Service Called AGAIN";
         }
 
         public string GetMochiIceCreamByFlavor(string flavor)
         {
-            return string.Format("Your {0} ice cream is served! - Internal Service Called", flavor);
+            return string.Format("Your {0} ice cream is served! - Internal Service Called AGAIN", flavor);
         }
     }
 }
