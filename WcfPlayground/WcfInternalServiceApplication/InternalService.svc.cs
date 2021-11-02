2f482efdb027f13745597bf29dece76a739102eb
emily.jimmerson <57643494+Emijii@users.noreply.github.com>
2021-11-02 11:37:22 -0600

    Updated the internal service then published the external service. The external service reflects the changes from the internal service automatically without having to update the service reference.

--- WcfPlayground/WcfInternalServiceApplication/InternalService.svc.cs
+++ WcfPlayground/WcfInternalServiceApplication/InternalService.svc.cs
@@ -4,12 +4,12 @@
     {
         public string GetMochi()
         {
-            return "Hi Mochi! :-) - Internal Service Called AGAIN";
+            return "Hi Mochi! :-) - Internal Service Called";
         }
 
         public string GetMochiIceCreamByFlavor(string flavor)
         {
-            return string.Format("Your {0} ice cream is served! - Internal Service Called AGAIN", flavor);
+            return string.Format("Your {0} ice cream is served! - Internal Service Called", flavor);
         }
     }
 }
